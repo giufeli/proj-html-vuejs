ca3bd2e22f599d8afd844dc3f55c955ab3d49e83
giufeli <giuliofelicetti99@gmail.com>
2022-12-05 17:39:55 +0100

    Start Project

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <HeaderPage :site-data="siteData" />
+    <MainPage />
+    <FooterPage />
+  </div>
+</template>
+
+<script>
+
+import HeaderPage from '@/components/HeaderPage.vue';
+import MainPage from '@/components/MainPage.vue';
+import FooterPage from '@/components/FooterPage.vue';
+
+export default {
+
+  components: {
+    HeaderPage,
+    MainPage,
+    FooterPage,
+  },
+
+  data() {
+    return {
+      siteData: [
+        {
+          email: 'cargotransport@gmail.com',
+          phoneNumber: '+1(305)1234-5678',
+          openHours: 'Mon - Sat - 9:00 - 18:00',
+          photo: [
+            {
+              banner1: 'bg-9.jpg',
+              banner2: 'bg-10.jpg',
+            },
+          ],
+        },
+      ],
+      veichelTypes: [
+        {
+          name: 'Light',
+          weight: '1200Kg',
+          img: 'truck-1.png',
+        },
+        {
+          name: 'Medium',
+          weight: '6000kg',
+          img: 'truck-1.png',
+        },
+        {
+          name: 'Heavy',
+          weight: '24000kg',
+          img: 'truck-1.png',
+        },
+
+      ],
+
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+  @import "../node_modules/bootstrap/scss/bootstrap";
+</style>
---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,72 @@
+<template>
+  <header>
+    <div class="topBar">
+      <div class="container">
+        <div class="row">
+          <div
+            v-for="obj in siteData"
+            :key="(obj +1)"
+            class="col-6"
+          >
+            {{ obj.openHours }}
+          </div>
+
+          <div
+            v-for="obj in siteData"
+            :key="(obj +2)"
+            class="col-6 d-flex justify-content-around gap-3"
+          >
+            <div>
+              {{ obj.phoneNumber }}
+            </div>
+            <div>
+              {{ obj.email }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div
+      v-for="obj in siteData"
+      :key="(obj +1)"
+      class="home bg-image"
+    >
+      <img
+        :src="`@/assets/img/${obj.photo[banner1]}`"
+        alt="banner1"
+      >
+      <div class="navBar">
+        ciao
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  props: {
+    siteData: Array,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .topBar{
+    width: 100%;
+    height: 45px;
+    background-color: rgba(33,51,62,255);
+    color: white;
+  }
+  .container, .row{
+    height: 100%;
+  }
+  .col-6{
+    margin: auto;
+  }
+
+  .home{
+    width: 100%;
+    height: 70vh;
+  }
+
+</style>
